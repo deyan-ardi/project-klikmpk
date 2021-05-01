d85ab15d2d7b225ea89674bb79137e9a45586902
deyan-ardi <riyan.clsg11@gmail.com>
2021-05-01 14:15:49 +0800

    Vendor Ditambahkan

---
+++ vendor/myth/auth/src/Views/master.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" class="h-100">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>KlikMPK - Halaman Authentifikasi</title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url(); ?>/assets/klik-icon.ico">
+    <link href="<?= base_url(); ?>/assets/vendor_admin/bootstrap-select/dist/css/bootstrap-select.min.css"
+        rel="stylesheet">
+    <link href="<?= base_url(); ?>/assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="vh-100">
+    <?php $this->renderSection('body') ?>
+
+
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <!-- Required vendor_admins -->
+    <script src="<?= base_url(); ?>/assets/vendor_admin/global/global.min.js"></script>
+    <script src="<?= base_url(); ?>/assets/vendor_admin/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+    <script src="<?= base_url(); ?>/assets/js/deznav-init.js"></script>
+    <script src="<?= base_url(); ?>/assets/js/demo.js"></script>
+</body>
+
+<!-- Mirrored from zenix.dexignzone.com/xhtml/page-login.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 19 Apr 2021 12:34:40 GMT -->
+
+</html>
