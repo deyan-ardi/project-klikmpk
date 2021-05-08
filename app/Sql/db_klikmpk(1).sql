993af9109b1de79439a49b8b6e718d84d0272154
ganatech id <ganatech.id@gmail.com>
2021-05-09 01:12:43 +0800

    Penambahan DB

--- app/Sql/db_klikmpk(1).sql
+++ app/Sql/db_klikmpk(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 04 Bulan Mei 2021 pada 13.31
+-- Waktu pembuatan: 08 Bulan Mei 2021 pada 17.11
 -- Versi server: 5.7.24
 -- Versi PHP: 7.4.16
 
@@ -40,7 +40,9 @@ CREATE TABLE `auth_activation_attempts` (
 --
 
 INSERT INTO `auth_activation_attempts` (`id`, `ip_address`, `user_agent`, `token`, `created_at`) VALUES
-(1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0', 'a1a72ae868859b1083b189732cde42f9', '2021-05-03 19:31:00');
+(1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0', 'a1a72ae868859b1083b189732cde42f9', '2021-05-03 19:31:00'),
+(2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0', '25bc74e2e1fdf4e1f2a87d364f83d7ad', '2021-05-08 00:48:20'),
+(3, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0', 'e1f547d4a378741db003a5724134b6f4', '2021-05-08 13:20:52');
 
 -- --------------------------------------------------------
 
@@ -53,6 +55,14 @@ CREATE TABLE `auth_groups` (
   `name` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `auth_groups`
+--
+
+INSERT INTO `auth_groups` (`id`, `name`, `description`) VALUES
+(1, 'member', 'for member user, default group'),
+(2, 'admin', 'for admin user, can access anyone fiture');
 
 -- --------------------------------------------------------
 
@@ -72,9 +82,19 @@ CREATE TABLE `auth_groups_permissions` (
 --
 
 CREATE TABLE `auth_groups_users` (
+  `id` int(11) NOT NULL,
   `group_id` int(11) UNSIGNED NOT NULL DEFAULT '0',
   `user_id` int(11) UNSIGNED NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `auth_groups_users`
+--
+
+INSERT INTO `auth_groups_users` (`id`, `group_id`, `user_id`) VALUES
+(4, 1, 4),
+(5, 1, 5),
+(1, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -104,7 +124,27 @@ INSERT INTO `auth_logins` (`id`, `ip_address`, `email`, `user_id`, `date`, `succ
 (6, '127.0.0.1', 'riyan@undiksha.ac.id', 1, '2021-05-04 18:35:06', 1),
 (7, '127.0.0.1', 'riyan.clsg11@gmail.com', NULL, '2021-05-04 18:52:13', 0),
 (8, '127.0.0.1', 'riyan.clsg11@gmail.com', 2, '2021-05-04 18:52:19', 0),
-(9, '127.0.0.1', 'riyan@undiksha.ac.id', 1, '2021-05-04 18:55:00', 1);
+(9, '127.0.0.1', 'riyan@undiksha.ac.id', 1, '2021-05-04 18:55:00', 1),
+(10, '127.0.0.1', 'riyan@undiksha.ac.id', 1, '2021-05-07 19:05:31', 1),
+(11, '127.0.0.1', 'ganatech.id@gmail.com', 2, '2021-05-08 00:48:35', 1),
+(12, '127.0.0.1', 'riyan.clsg11@gmail.com', NULL, '2021-05-08 12:06:51', 0),
+(13, '127.0.0.1', 'ganatech.id@gmail.com', 2, '2021-05-08 12:06:56', 1),
+(14, '127.0.0.1', 'riyan.clsg11@gmail.com', 3, '2021-05-08 13:20:59', 1),
+(15, '127.0.0.1', 'ganatech.id@gmail.com', 2, '2021-05-08 13:22:48', 1),
+(16, '127.0.0.1', 'riyan.clsg11@gmail.com', 3, '2021-05-08 13:28:11', 1),
+(17, '127.0.0.1', 'ganatech.id@gmail.com', 2, '2021-05-08 20:46:41', 1),
+(18, '127.0.0.1', 'riyan@undiksha.ac.id', 1, '2021-05-08 20:53:12', 1),
+(19, '127.0.0.1', 'ganatech.id@gmail.com', 2, '2021-05-08 20:57:42', 1),
+(20, '127.0.0.1', 'riyan.clsg13@gmail.com', NULL, '2021-05-08 22:18:36', 0),
+(21, '127.0.0.1', 'riyan.clsg11@gmail.com', 3, '2021-05-08 22:18:40', 1),
+(22, '127.0.0.1', 'riyan@undiksha.ac.id', 1, '2021-05-08 22:18:58', 1),
+(23, '127.0.0.1', 'riyan.clsg11@gmail.com', 3, '2021-05-08 22:52:24', 1),
+(24, '127.0.0.1', 'ganatech.id@gmail.com', 2, '2021-05-08 22:52:30', 1),
+(25, '127.0.0.1', 'riyan.clsg11@gmail.com', 3, '2021-05-08 22:56:06', 1),
+(26, '127.0.0.1', 'ganatech.id@gmail.com', 2, '2021-05-08 22:56:20', 1),
+(27, '::1', 'ganatech.id@gmail.com', 2, '2021-05-08 22:56:39', 1),
+(28, '::1', 'riyan@undiksha.ac.id', 1, '2021-05-08 22:56:53', 1),
+(29, '127.0.0.1', 'riyan@undiksha.ac.id', 1, '2021-05-09 00:53:34', 1);
 
 -- --------------------------------------------------------
 
@@ -157,6 +197,23 @@ CREATE TABLE `auth_users_permissions` (
   `user_id` int(11) UNSIGNED NOT NULL DEFAULT '0',
   `permission_id` int(11) UNSIGNED NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `bank_soal`
+--
+
+CREATE TABLE `bank_soal` (
+  `id_bank_soal` int(11) NOT NULL,
+  `kode_soal` varchar(15) NOT NULL,
+  `kategori_soal` varchar(150) NOT NULL,
+  `mata_kuliah` varchar(150) NOT NULL,
+  `deskripsi_soal` text NOT NULL,
+  `file_pdf` varchar(150) NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -242,6 +299,14 @@ CREATE TABLE `kelas` (
   `created_by` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `id_user`, `sampul_kelas`, `nama_kelas`, `semester`, `mata_kuliah`, `bagikan`, `tautan`, `n_seluruh`, `n_tugas`, `n_uts`, `n_uas`, `n_sikap`, `created_at`, `created_by`) VALUES
+(2, 5, NULL, 'Rombel45', 1, 'MPK Bahasa Indonesia', 0, NULL, 0, 0, 0, 0, 0, '2021-05-08 17:08:37', 'I NYOMAN SUDIANA'),
+(3, 4, NULL, 'Rombel43', 2, 'MPK Bahasa Indonesia', 0, NULL, 0, 0, 0, 0, 0, '2021-05-08 17:08:37', 'Nengah Suandi');
+
 -- --------------------------------------------------------
 
 --
@@ -278,6 +343,21 @@ CREATE TABLE `migrations` (
   `time` int(11) NOT NULL,
   `batch` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `request_unduh`
+--
+
+CREATE TABLE `request_unduh` (
+  `id_request` int(11) NOT NULL,
+  `id_bank_soal` int(11) NOT NULL,
+  `id_user` int(10) UNSIGNED NOT NULL,
+  `status_unduh` int(11) NOT NULL DEFAULT '0',
+  `pesan_pengajuan` text,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -359,7 +439,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `username`, `foto_profil`, `foto_sampul`, `deskripsi`, `password_hash`, `reset_hash`, `reset_at`, `reset_expires`, `activate_hash`, `status`, `status_message`, `active`, `force_pass_reset`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'riyan@undiksha.ac.id', 'deyan ardi', NULL, NULL, NULL, '$2y$10$2hAY/oqJE.u1H5P/OU1cg.kR50JvNNrL/Ibsr6EbehWk.V8bfQqL.', NULL, NULL, NULL, NULL, NULL, NULL, 1, 0, '2021-05-03 19:30:45', '2021-05-03 19:31:00', NULL);
+(1, 'riyan@undiksha.ac.id', 'deyan ardi', NULL, NULL, NULL, '$2y$10$2hAY/oqJE.u1H5P/OU1cg.kR50JvNNrL/Ibsr6EbehWk.V8bfQqL.', NULL, NULL, NULL, NULL, NULL, NULL, 1, 0, '2021-05-03 19:30:45', '2021-05-07 20:49:52', NULL),
+(4, 'gek_chrysan@yahoo.co.id', 'Nengah Suandi', NULL, NULL, NULL, '$2y$10$.nDsJO3emrjCTywtx5AJAOqoYntSwyLLjHNYnmY6B/wcVVFKhy/XW', NULL, NULL, NULL, '', NULL, NULL, 1, 0, '2021-05-08 16:56:45', '2021-05-08 16:56:45', NULL),
+(5, 'sudiana195723@gmail.com', 'I NYOMAN SUDIANA', NULL, NULL, NULL, '$2y$10$2o.89TSYFdJTvSPq4fFhWuBp9VdgFEkYHHJvx7iwaEHBayvW1jk0u', NULL, NULL, NULL, NULL, NULL, NULL, 1, 0, '2021-05-08 16:56:45', '2021-05-08 16:56:45', NULL);
 
 -- --------------------------------------------------------
 
@@ -402,6 +484,7 @@ ALTER TABLE `auth_groups_permissions`
 -- Indeks untuk tabel `auth_groups_users`
 --
 ALTER TABLE `auth_groups_users`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `auth_groups_users_user_id_foreign` (`user_id`),
   ADD KEY `group_id_user_id` (`group_id`,`user_id`);
 
@@ -439,6 +522,12 @@ ALTER TABLE `auth_tokens`
 ALTER TABLE `auth_users_permissions`
   ADD KEY `auth_users_permissions_permission_id_foreign` (`permission_id`),
   ADD KEY `user_id_permission_id` (`user_id`,`permission_id`);
+
+--
+-- Indeks untuk tabel `bank_soal`
+--
+ALTER TABLE `bank_soal`
+  ADD PRIMARY KEY (`id_bank_soal`);
 
 --
 -- Indeks untuk tabel `detail_kelas`
@@ -489,6 +578,14 @@ ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `request_unduh`
+--
+ALTER TABLE `request_unduh`
+  ADD PRIMARY KEY (`id_request`),
+  ADD KEY `fk_request_bank` (`id_bank_soal`),
+  ADD KEY `fk_request_user` (`id_user`);
+
+--
 -- Indeks untuk tabel `sikap_partisipasi`
 --
 ALTER TABLE `sikap_partisipasi`
@@ -535,19 +632,25 @@ ALTER TABLE `uts`
 -- AUTO_INCREMENT untuk tabel `auth_activation_attempts`
 --
 ALTER TABLE `auth_activation_attempts`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `auth_groups`
 --
 ALTER TABLE `auth_groups`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `auth_groups_users`
+--
+ALTER TABLE `auth_groups_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `auth_logins`
 --
 ALTER TABLE `auth_logins`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT untuk tabel `auth_permissions`
@@ -568,40 +671,46 @@ ALTER TABLE `auth_tokens`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT untuk tabel `bank_soal`
+--
+ALTER TABLE `bank_soal`
+  MODIFY `id_bank_soal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT untuk tabel `detail_kelas`
 --
 ALTER TABLE `detail_kelas`
-  MODIFY `id_detail_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_detail_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT untuk tabel `kegiatan_tugas`
 --
 ALTER TABLE `kegiatan_tugas`
-  MODIFY `id_kegiatan_tugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_kegiatan_tugas` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `kegiatan_uas`
 --
 ALTER TABLE `kegiatan_uas`
-  MODIFY `id_kegiatan_uas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_kegiatan_uas` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `kegiatan_uts`
 --
 ALTER TABLE `kegiatan_uts`
-  MODIFY `id_kegiatan_uts` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_kegiatan_uts` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `kelas`
 --
 ALTER TABLE `kelas`
-  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  MODIFY `id_mahasiswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_mahasiswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT untuk tabel `migrations`
@@ -610,34 +719,40 @@ ALTER TABLE `migrations`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT untuk tabel `request_unduh`
+--
+ALTER TABLE `request_unduh`
+  MODIFY `id_request` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
 -- AUTO_INCREMENT untuk tabel `sikap_partisipasi`
 --
 ALTER TABLE `sikap_partisipasi`
-  MODIFY `id_sikap_partisipasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_sikap_partisipasi` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `tugas`
 --
 ALTER TABLE `tugas`
-  MODIFY `id_tugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_tugas` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `uas`
 --
 ALTER TABLE `uas`
-  MODIFY `id_uas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_uas` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `uts`
 --
 ALTER TABLE `uts`
-  MODIFY `id_uts` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_uts` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -700,6 +815,13 @@ ALTER TABLE `kegiatan_uts`
 --
 ALTER TABLE `kelas`
   ADD CONSTRAINT `fk_user_kelas` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `request_unduh`
+--
+ALTER TABLE `request_unduh`
+  ADD CONSTRAINT `fk_request_bank` FOREIGN KEY (`id_bank_soal`) REFERENCES `bank_soal` (`id_bank_soal`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_request_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `sikap_partisipasi`
